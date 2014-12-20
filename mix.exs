7c213c645a1db6cd4db93d416276554f7ec28a5b
Michael Truog <mjtruog@gmail.com>
2014-12-20 15:51:34 -0800

    Version 1.4.0.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule CloudIServiceDbCouchdb do
 
   def project do
     [app: :cloudi_service_db_couchdb,
-     version: "1.4.0-rc.4",
+     version: "1.4.0",
      language: :erlang,
      description: description,
      package: package,
@@ -14,7 +14,7 @@ defmodule CloudIServiceDbCouchdb do
     [{:ecouchdb,
       [git: "https://github.com/okeuday/ecouchdb.git",
        branch: "master"]},
-     {:cloudi_core, "~> 1.4.0-rc.4"}]
+     {:cloudi_core, "~> 1.4.0"}]
   end
 
   defp description do
